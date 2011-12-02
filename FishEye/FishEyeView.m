f752e37c0e4f19a68fbbb84dba9edf438430cc6d
haiwei <lihw@bitauto.com>
2011-12-02 14:22:45 +0800

    fix bug of top 3 unnormal show

--- FishEye/FishEyeView.m
+++ FishEye/FishEyeView.m
@@ -14,6 +14,8 @@
 
 - (void)resetPosition;
 
+- (void)calFishEyeWithPosition:(CGPoint)position 
+                  withAnimated:(BOOL)animated;
 @end
 
 @implementation FishEyeView (hide)
@@ -38,6 +40,82 @@
     
 }
 
+- (void)calFishEyeWithPosition:(CGPoint)position 
+                  withAnimated:(BOOL)animated{
+    
+    float _disc;
+    float _per;
+    float _rate;
+    
+    int indexTmp = -1;
+    
+    if(position.y < alphabets2[0].frame.origin.y || position.y > alphabets2[EYE_COUNT - 1].frame.origin.y + alphabets2[EYE_COUNT - 1].frame.size.height){
+        [self resetPosition];
+        return;
+    }
+    
+    for (int i = 0; i < EYE_COUNT; ++i){
+        if (position.y > alphabets2[i].frame.origin.y && position.y <= alphabets2[i].frame.origin.y + m_Height ) {
+            indexTmp = i;
+            _per = (position.y - alphabets2[i].frame.origin.y) * m_MaxRate;
+            break;
+        }
+    }
+    
+    if (indexTmp == -1) {
+        return;
+    }
+    
+    if (animated) {
+        [UIView beginAnimations: @"FishEyeAnimation" context: NULL];
+        [UIView setAnimationCurve: UIViewAnimationCurveEaseInOut];
+        [UIView setAnimationDuration: 0.3];
+    }
+    
+    
+    alphabets[indexTmp].frame = CGRectMake(0, position.y - _per, m_Width * m_MaxRate, m_Height * m_MaxRate);
+    
+    for (int i = indexTmp - 1; i >= 0; --i) {
+
+        if (i < (indexTmp - 3)) {
+
+            alphabets[i].frame = CGRectMake(0, alphabets2[indexTmp].center.y - m_Height * abs(i - indexTmp) - m_Offset - m_Height, m_Width, m_Height);
+        }else{
+            
+            _disc = abs(alphabets2[i].center.y - position.y );
+            
+            _rate = m_A * _disc + m_B;
+            
+            alphabets[i].frame = CGRectMake(0, alphabets[i + 1].frame.origin.y - m_Height * _rate, m_Width * _rate, m_Height * _rate);
+        }        
+    }
+    
+    for (int i = indexTmp + 1; i < EYE_COUNT; ++i) {
+        
+        if (i > indexTmp + 3) {
+            
+            alphabets[i].frame = CGRectMake(0, alphabets2[indexTmp].center.y + m_Height * abs(i - indexTmp) + m_Offset, m_Width, m_Height);
+        }else{
+            
+            _disc = abs(alphabets2[i].center.y - position.y );
+            
+            _rate = m_A * _disc + m_B;  
+            alphabets[i].frame = CGRectMake(0, alphabets[i - 1].frame.origin.y + alphabets[i - 1].frame.size.height, m_Width * _rate, m_Height * _rate);
+        }        
+    }
+    
+    if (animated) {
+        [UIView commitAnimations];
+    }
+    
+    if (index != indexTmp && indexDelegate && [indexDelegate respondsToSelector: @selector(fishEyeIndex:)])
+    {
+        index = indexTmp;
+        [indexDelegate fishEyeIndex : index];
+    }
+    
+}
+
 @end
 
 
@@ -46,7 +124,6 @@
 @synthesize indexDelegate;
 
 - (void)dealloc{
-    
     
     
     [super dealloc];
@@ -58,7 +135,6 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        
         
     }
     return self;
@@ -104,76 +180,7 @@
     
 }
 
-- (void)calFishEyeWithPosition:(CGPoint)position 
-                  withAnimated:(BOOL)animated{
-    
-    float _disc;
-    float _per;
-    float _rate;
-    
-    NSUInteger indexTmp;
-    
-    for (int i = 0; i < EYE_COUNT; ++i){
-        if (position.y > alphabets2[i].frame.origin.y && position.y <= alphabets2[i].frame.origin.y + m_Height ) {
-            indexTmp = i;
-            _per = (position.y - alphabets2[i].frame.origin.y) * m_MaxRate;
-            break;
-        }else if(i == 0){
-            if(position.y < alphabets2[i].frame.origin.y){
-                [self resetPosition];
-                return;
-            }
-        }else if(i == EYE_COUNT - 1){
-            if(position.y > alphabets2[i].frame.origin.y + alphabets2[i].frame.size.height){
-                [self resetPosition];
-                return;
-            }
-        }
-    }
-    
-    if (animated) {
-        [UIView beginAnimations: @"FishEyeAnimation" context: NULL];
-        [UIView setAnimationCurve: UIViewAnimationCurveEaseInOut];
-        [UIView setAnimationDuration: 0.3];
-    }
-    
-        
-    alphabets[indexTmp].frame = CGRectMake(0, position.y - _per, m_Width * m_MaxRate, m_Height * m_MaxRate);
-        
-    for (int i = indexTmp - 1; i > -1; i--) {
-        if (i < indexTmp - 3) {
-            alphabets[i].frame = CGRectMake(0, alphabets2[indexTmp].center.y - m_Height * abs(i - indexTmp) - m_Offset - m_Height, m_Width, m_Height);
-        }else{
-            _disc = abs(alphabets2[i].center.y - position.y );
-            
-            _rate = m_A * _disc + m_B;
-            alphabets[i].frame = CGRectMake(0, alphabets[i + 1].frame.origin.y - m_Height * _rate, m_Width * _rate, m_Height * _rate);
-        }
-        
-    }
-    for (int i = indexTmp + 1; i < EYE_COUNT; i++) {
-        if (i > indexTmp + 3) {
-            alphabets[i].frame = CGRectMake(0, alphabets2[indexTmp].center.y + m_Height * abs(i - indexTmp) + m_Offset, m_Width, m_Height);
-        }else{
-            _disc = abs(alphabets2[i].center.y - position.y );
-                        
-            _rate = m_A * _disc + m_B;  
-            alphabets[i].frame = CGRectMake(0, alphabets[i - 1].frame.origin.y + alphabets[i - 1].frame.size.height, m_Width * _rate, m_Height * _rate);
-        }        
-    }
-    if (animated) {
-        [UIView commitAnimations];
-    }
-    
-    if (index != indexTmp && indexDelegate && [indexDelegate respondsToSelector: @selector(fishEyeIndex:)])
-    {
-        index = indexTmp;
-        [indexDelegate fishEyeIndex : index];
-    }
-
-}
-
-
+#pragma mark - touch method
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
